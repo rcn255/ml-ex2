a89173f28ce01a18f7806124a05538ea504caf6e
JB <janick255@gmail.com>
2024-05-17 18:58:01 +0200

    Implemented gradient descent for any dataset with RSS

--- janick/gradient_descent.py
+++ janick/gradient_descent.py
@@ -10,8 +10,13 @@ class GDRegressor:
         self.optimal_w = 0
 
     def fit(self, X, y):
-        X_b = np.c_[np.ones((100, 1)), X]
-        self.gradient_descent(X_b, y, w)
+        print(f"X shape: {X.shape}")
+        print(f"y shape: {y.shape}")
+        w = np.random.randn(X.shape[1] + 1)
+        print(f"w: {w}")
+        X_bias = np.c_[np.ones((X.shape[0], 1)), X]
+        print(f"X_bias shape: {X_bias.shape}")
+        self.gradient_descent(X_bias, y, w)
 
     # RSS cost function
     def RSS(self, X, y, w):
@@ -30,17 +35,36 @@ class GDRegressor:
     # Gradient descent
     def gradient_descent(self, X, y, w):
         for i in range(self.max_iter):
-            w = w - self.learning_rate * self.RSS_gradient(X, y, w)
+            old_w = w
+            gradient_result = self.RSS_gradient(X, y, w)
+            w = w - self.learning_rate * gradient_result
+            #print(f"gradient_result: {gradient_result}")
+            print(f"Itaration {i}:\n New w: {w}\n Gradient: {gradient_result}")
+
             self.history.append((w.copy(), self.RSS(X, y, w)))
             self.optimal_w = w
         return
+
+    def predict(self, X):
+        #print(self.optimal_w.shape)
+        X_bias = np.c_[np.ones((X.shape[0], 1)), X]
+        #print(X_bias)
+        return X_bias.dot(self.optimal_w)
+
+    def plot_cost_history(self):
+        # Plot the cost function history
+        costs = [h[1] for h in self.history]
+        plt.plot(costs)
+        plt.xlabel('Iteration')
+        plt.ylabel('Cost')
+        plt.title('Cost Function History')
+        plt.show()
 
 if __name__ == '__main__':
     np.random.seed(0)
     X = 2 * np.random.rand(100, 2)
     y = 4 + 3 * X[:, 0] + 5 * X[:, 1] + np.random.randn(100)
 
-    w = np.random.randn(3)
     learning_rate = 0.1
     max_iter = 20
 
@@ -48,12 +72,5 @@ if __name__ == '__main__':
 
     model.fit(X, y)
 
-    # Plot the cost function history
-    costs = [h[1] for h in model.history]
-    plt.plot(costs)
-    plt.xlabel('Iteration')
-    plt.ylabel('Cost')
-    plt.title('Cost Function History')
-    plt.show()
 
     print(f'Optimal parameters: {model.optimal_w}')
